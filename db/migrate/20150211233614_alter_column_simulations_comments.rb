d357ca8ff4e5f40469bfadeee5dc6dc3ad4fb3c9
Tevin Steinke <TSteinke@networkats.com>
2015-02-11 18:46:05 -0500

    fix migration (on heroku we will need to drop and recreate the database)

--- db/migrate/20150211233614_alter_column_simulations_comments.rb
+++ db/migrate/20150211233614_alter_column_simulations_comments.rb
@@ -1,11 +1,11 @@
 class AlterColumnSimulationsComments < ActiveRecord::Migration
   def self.up
-    change_table :products do |t|
+    change_table :simulations do |t|
       t.change :comments, :text
     end
   end
-  def self.up
-    change_table :products do |t|
+  def self.down
+    change_table :simulations do |t|
       t.change :comments, :string
     end
   end
